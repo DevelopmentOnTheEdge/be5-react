d46fec31aaef8c80fbc6d9492c9a93fe3d5dc158
uuinnk <uuinnk@developmentontheedge.com>
2017-05-30 15:07:45 +0700

    ES6 module

--- src/scripts/be5/components/staticPage.js
+++ src/scripts/be5/components/staticPage.js
@@ -1,20 +1,21 @@
 import React          from 'react';
+import PropTypes from 'prop-types';
 import be5            from 'be5/be5';
 import changeDocument from 'be5/core/changeDocument';
 
-const StaticPage = React.createClass({
-  propTypes: {
-    value: React.PropTypes.string.isRequired
-  },
-  
-  displayName: 'StaticPage',
-  
+class StaticPage extends React.Component {
+
   render() {
     var content = this.props.value;
     be5.ui.convertLinks(content);
     return React.DOM.div({ className: 'staticPage', dangerouslySetInnerHTML: { __html: content } });
   }
-});
+
+}
+
+StaticPage.propTypes = {
+  value: PropTypes.string.isRequired
+};
 
 be5.ui.registerDocumentType('static', function(value) {
   return React.createElement(StaticPage, value);
