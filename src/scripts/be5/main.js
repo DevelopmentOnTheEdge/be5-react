73e0505cfa3672d117d682b32cb18bcdbc451a98
uuinnk <uuinnk@developmentontheedge.com>
2017-08-03 18:30:37 +0700

    fix styles

--- src/scripts/be5/main.js
+++ src/scripts/be5/main.js
@@ -4,11 +4,13 @@ import App              from './components/application';
 
 import 'react-datetime/css/react-datetime.css';
 import 'react-select/dist/react-select.css';
-import '../../sass/styles.scss'
-import '../../css/formWizard.css';
 import 'react-virtualized/styles.css'
 import 'react-virtualized-select/styles.css'
 import "beanexplorer-react/src/propertySet.css";
+
+import '../../sass/styles.scss'
+import '../../css/formWizard.css';
+import '../../css/main.css';
 
 //todo move to egisso-be5 frontend
 import EgissoDocsForm from './forms/egissoDocsForm';
