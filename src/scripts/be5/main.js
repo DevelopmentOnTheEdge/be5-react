12893b59acab91fa513d7683bced712e5a23ff57
uuinnk <uuinnk@developmentontheedge.com>
2017-08-07 12:35:06 +0700

    fix

--- src/scripts/be5/main.js
+++ src/scripts/be5/main.js
@@ -1,7 +1,7 @@
 import ReactDOM         from 'react-dom';
 import React            from 'react';
 import App              from './components/application';
-import App              from './importActions';
+import ImportActions              from './importActions';
 
 import 'react-datetime/css/react-datetime.css';
 import 'react-select/dist/react-select.css';
