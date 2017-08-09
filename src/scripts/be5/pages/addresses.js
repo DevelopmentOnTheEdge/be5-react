0802272ce2fe2ac35a5689d009496b9432030f5c
uuinnk <uuinnk@developmentontheedge.com>
2017-08-09 16:46:41 +0700

    Организация - Адреса

--- src/scripts/be5/pages/addresses.js
+++ src/scripts/be5/pages/addresses.js
@@ -13,34 +13,40 @@ class Addresses extends React.Component {
   }
 
   componentDidMount(){
-    be5.url.process("Addresses1", "#!form/_test_/Test%201D/InsertOccupancy");
+    be5.url.process("table", "#!table/occupancies/For companies");
+    be5.url.process("form", "#!form/_test_/Test%201D/InsertOccupancy");
   }
 
   render() {
     return (
-      <div className="row"><div className="container max-width-970">
-        <button className="btn btn-info pull-right btn-sm float-right" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-            Справка
-        </button>
-        <h1>Организация - Адреса</h1>
-        <div className="collapse" id="collapse1">
-          <div className="alert alert-success" role="alert">
-            <p>Заполните сведения об адресах организации и адресах мест обращения за услугами.<br/>
-               Обязательно необходимо заполнить сведения об адресах следующего типа:</p>
-            <ul>
-              <li>Юридический адрес</li>
-              <li>Фактический адрес</li>
-              <li>Место подачи заявлений</li>
-            </ul>
-            Адресов с типом “Место подачи заявлений” может быть несколько.<br/>
-            При создании адреса заполните последовательно поля: Дата начала, Регион, Район (при необходимости), Город (при необходимости), Населенный пункт (при необходимости), Улица, Дом, Квартира.<br/>
-            Если в списке домов и квартир нет нужных, добавьте их с помощью операции “Добавить”.<br/>
-            Если адреса совпадают, выполните операцию “Копия записи”, изменив только тип адреса.
+      <div>
+        <Document documentName={"table"} operationDocumentName={"form"}/>
+        <div className="row"><div className="col-xs-12 max-width-970">
+          <button className="btn btn-info pull-right btn-sm float-right" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+              Справка
+          </button>
+          <h1>&nbsp;</h1>
+          <div className="collapse" id="collapse1">
+            <div className="alert alert-success" role="alert">
+              <p>Заполните сведения об адресах организации и адресах мест обращения за услугами.<br/>
+                 Обязательно необходимо заполнить сведения об адресах следующего типа:</p>
+              <ul>
+                <li>Юридический адрес</li>
+                <li>Фактический адрес</li>
+                <li>Место подачи заявлений</li>
+              </ul>
+              Адресов с типом “Место подачи заявлений” может быть несколько.<br/>
+              При создании адреса заполните последовательно поля: Дата начала, Регион, Район (при необходимости), Город (при необходимости), Населенный пункт (при необходимости), Улица, Дом, Квартира.<br/>
+              Если в списке домов и квартир нет нужных, добавьте их с помощью операции “Добавить”.<br/>
+              Если адреса совпадают, выполните операцию “Копия записи”, изменив только тип адреса.
+            </div>
           </div>
-        </div>
 
-        <Document documentName={"Addresses1"} />
-      </div></div>
+          <Document documentName={"form"} onChange={()=>{
+            be5.url.process("table", "#!table/occupancies/For companies");
+          }} />
+        </div></div>
+      </div>
     );
   }
 
