e06ddbdb6d0555b43bc00c44348752f81411a3dd
uuinnk <uuinnk@developmentontheedge.com>
2017-08-11 12:03:22 +0700

    tabs

--- src/scripts/be5/pages/egissoDocs.js
+++ src/scripts/be5/pages/egissoDocs.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import be5                  from '../be5';
 import Document             from '../components/document';
 import changeDocument from '../core/changeDocument';
+import Navs             from '../components/navs';
 
 class EgissoDocs extends React.Component {
 
@@ -17,47 +18,53 @@ class EgissoDocs extends React.Component {
 
   }
 
-  render() {
+  help(){
     return (
-      <div>
-        <div className="row">
-          <div className="col-md-3"><Document documentName={"categories"} /></div>
-          <div className="col-md-3"><Document documentName={"docTypes"} /></div>
-          <div className="col-md-6"><Document documentName={"classifications"} /></div>
-        </div>
+      <div className="col-md-12">
+        <button className="btn btn-info pull-right btn-sm float-right" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+            Справка
+        </button>
+        <h1>Документы</h1>
+        <div className="collapse" id="collapse1"><div className="alert alert-success" role="alert">
+          Заполните сведения о документах, которые требуются при определении права на предоставление услуг.
+          Категории документов необходимы для того, чтобы группировать взаимозаменяемые документы, которые
+          может предоставить гражданин при обращении за предоставлением услуги.
+          По умолчанию для примера добавлены следующе категории документов:
+            <ol>
+              <li>
+                  Категория “Документ, удостоверяющий личность граждан РФ”. К ней относятся следующие документы:
+                  <ol className="lower-alpha">
+                      <li>Паспорт гражданина РФ</li>
+                      <li>Свидетельство о рождении</li>
+                      <li>Военный билет</li>
+                      <li>Удостоверение личности военнослужащего</li>
+                      <li>Иной документ, удостоверяющий личность</li>
+                  </ol>
+              </li>
+
+              <li>
+                  Категория “Документы, подтверждающие доходы”. К ней относятся следующие документы:
+                  <ol className="lower-alpha">
+                      <li>Справка о заработной плате</li>
+                  </ol>
+              </li>
+          </ol>
+        </div></div>
+      </div>
+    )
+  }
+
+  render() {
+    const help = this.help();
+    const steps = [
+      {title: 'Категории документов', url: '#!table/categories/All records'},
+      {title: 'Документы', url: '#!table/docTypes/All records'},
+      {title: 'Отнесение документов к категориям', url: '#!table/classifications/All records'}
+    ];
+    return (<div>
       <div className="row">
       <div className="container max-width-970"><div className="row">
-        <div className="col-md-12">
-          <button className="btn btn-info pull-right btn-sm float-right" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-              Справка
-          </button>
-          <h1>Документы</h1>
-          <div className="collapse" id="collapse1"><div className="alert alert-success" role="alert">
-            Заполните сведения о документах, которые требуются при определении права на предоставление услуг.
-            Категории документов необходимы для того, чтобы группировать взаимозаменяемые документы, которые
-            может предоставить гражданин при обращении за предоставлением услуги.
-            По умолчанию для примера добавлены следующе категории документов:
-            <ol>
-                <li>
-                    Категория “Документ, удостоверяющий личность граждан РФ”. К ней относятся следующие документы:
-                    <ol className="lower-alpha">
-                        <li>Паспорт гражданина РФ</li>
-                        <li>Свидетельство о рождении</li>
-                        <li>Военный билет</li>
-                        <li>Удостоверение личности военнослужащего</li>
-                        <li>Иной документ, удостоверяющий личность</li>
-                    </ol>
-                </li>
-
-                <li>
-                    Категория “Документы, подтверждающие доходы”. К ней относятся следующие документы:
-                    <ol className="lower-alpha">
-                        <li>Справка о заработной плате</li>
-                    </ol>
-                </li>
-            </ol>
-          </div></div>
-        </div>
+        {help}
         <div className="col-md-12">
           <p>Для начала создайте категории документов, которые необходимы для предоставления услуг в Вашем муниципалитете.</p>
         </div>
@@ -93,8 +100,9 @@ class EgissoDocs extends React.Component {
         </div>
 
       </div></div></div>
-      </div>
-    );
+      <br/>
+      <Navs steps={steps} tabs/>
+    </div>);
   }
 
 //        <div className="row"><div className="col-md-12">
