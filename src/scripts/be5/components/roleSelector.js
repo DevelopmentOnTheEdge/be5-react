9840db60fa492847b2eac127eb77ac2cf193c07d
Innokentiy <uu.innk@gmail.com>
2017-10-02 23:32:00 +0700

    roleSelector snapshot test

--- src/scripts/be5/components/roleSelector.js
+++ src/scripts/be5/components/roleSelector.js
@@ -1,77 +1,86 @@
-import React from 'react';
 import be5 from '../be5';
 import bus from '../core/bus';
+import React, { Component } from 'react';
+import PropTypes            from 'prop-types';
 
 import '../../../css/roleSelector.css';
 
-const Role = React.createClass({
-  displayName: 'Role',
-  
-  propTypes: {
-    onChange: React.PropTypes.func.isRequired
-  },
-  
-  getInitialState() {
-    return { selectedRoles : this.props.selectedRoles };
-  },
-  
+class Role extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {selectedRoles : this.props.selectedRoles};
+
+    this._onChange = this._onChange.bind(this);
+  }
+
   render() {
-    var id = this.props.name + "-checkbox";
+    const id = this.props.name + "-checkbox";
     return (
-      React.DOM.div({className: "role"}, 
-        React.DOM.input({type: "checkbox", checked: this.state.selectedRoles, id: id, onChange: this._onChange}), 
-        React.DOM.label({htmlFor: id}, React.DOM.span({className: "checkBox"}), this.props.name)
-      )
-    );
-  },
-  
+      <div className={"role"}>
+        <input type="checkbox" id={id} checked={this.state.selectedRoles} onChange={this._onChange} />
+        <label htmlFor={id}><span className={"checkBox"}/>{this.props.name}</label>
+      </div>
+    )
+  }
+
   _onChange(e) {
     this.setState({ selectedRoles : e.target.checked }, this.props.onChange);
   }
-});
+}
 
-export default React.createClass({
-  displayName: 'RoleBox',
-  
-  getInitialState() {
+Role.propTypes = {
+  onChange: PropTypes.func.isRequired
+};
+
+class RoleBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {availableRoles: ["Unknown"], selectedRoles: ["Unknown"]};
+    this._onRoleChange = this._onRoleChange.bind(this);
+    this._changeRoles = this._changeRoles.bind(this);
+  }
+
+  static getInitialState() {
     return { availableRoles: ["Unknown"], selectedRoles: ["Unknown"] };
-  },
-  
+  }
+
   render() {
     if (this.state.availableRoles.length <= 1) {
-      return ( React.DOM.div({className: "roleBox"}) );
+      return ( <div className={'roleBox'}/> );
     }
-    var selectedRoles = this.state.selectedRoles;
-    var roleNodes = this.state.availableRoles.map(function (role) {
-      return (
-        React.createElement(Role, {key: role, ref: role, name: role, selectedRoles: $.inArray(role, selectedRoles) != -1, onChange: this._onRoleChange})
-      );
-    }.bind(this));
-    return (
-      React.DOM.div({className: "roleBox"}, 
-        React.DOM.h4({}, be5.messages.roles), 
-        roleNodes
-      )
+    const selectedRoles = this.state.selectedRoles;
+    const roleNodes = this.state.availableRoles.map((role) =>
+      <Role key={role} ref={role} name={role} selectedRoles={$.inArray(role, selectedRoles) != -1} onChange={this._onRoleChange}/>
     );
-  },
-  
+    return (
+      <div className={'roleBox'}>
+        <h4>{be5.messages.roles}</h4>
+        {roleNodes}
+      </div>
+    );
+  }
+
   componentDidMount() {
     this.refresh();
-  },
-  
+  }
+
   refresh() {
     be5.net.request('roleSelector', {}, data => this.setState(data));
-  },
-  
+  }
+
   _onRoleChange() {
-    var roles = this.state.availableRoles.filter(name => this.refs[name].state.selectedRoles);
+    let roles = this.state.availableRoles.filter(name => this.refs[name].state.selectedRoles);
     this._changeRoles(roles.join(","));
-  },
-  
+  }
+
   _changeRoles(roles) {
     be5.net.request('roleSelector/select', { roles: roles }, data => {
       this.setState(data);
       bus.fire('RoleChanged', {});
     });
   }
-});
+}
+
+export default RoleBox;
---
+++ test/scripts/be5/components/roleSelector.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import RoleBox, {Role} from '../../../../src/scripts/be5/components/roleSelector.js';
+import be5 from '../../../../src/scripts/be5/be5';
+import {shallow, mount} from 'enzyme';
+
+
+it('render', () => {
+    be5.net.request = function (path, attr, callback) {
+      callback({"availableRoles":["Guest", "Administrator"],"selectedRoles":["Guest"]})
+    };
+
+    const component = renderer.create(
+        <RoleBox />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+});
+
+//it('calls', () => {
+//    const handle = be5.net.request = jest.fn();
+//
+//    const wrapper = mount(
+//        <RoleBox />
+//    );
+//    //wrapper.find(Role).last().simulate('click')
+//    expect(wrapper.find('.roleBox').length).toEqual(1);
+//    expect(wrapper.find('input').length).toEqual(1);
+//    //wrapper.find('#Administrator-checkbox').simulate('click');
+//
+//    expect(handle.mock.calls.length).toBe(1);
+////    expect(handle.mock.calls[0]).toEqual(["/testName", "My new value"]);
+//
+//});
