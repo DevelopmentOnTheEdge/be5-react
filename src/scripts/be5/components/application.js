57d380b7501584d0882e9493c2319a02de083d94
uuinnk <uuinnk@developmentontheedge.com>
2017-10-20 12:54:31 +0700

    revert AlertContainer

--- src/scripts/be5/components/application.js
+++ src/scripts/be5/components/application.js
@@ -2,38 +2,36 @@ import React from 'react';
 import bus from '../core/bus';
 import SideBar from './sideBar';
 import Document from './document';
-import SplitPane from 'react-split-pane';
-import Alert from 'react-s-alert';
+import SplitPane from './splitPane';
+import AlertContainer from 'react-alert'
 
 export default React.createClass({
   displayName: 'App',
-  
+
   componentDidMount: function() {
     bus.listen('LoggedOut', this.refresh);
     bus.listen('LoggedIn', this.refresh);
     bus.listen('LanguageChanged', this.refresh);
     bus.listen('RoleChanged', this.refresh);
     bus.listen("alert", data => {
-      if (data.type === "success") {
-        Alert.success(data.msg, {
-          position: 'top-right',
-          effect: 'slide',
-        });
-      } else {
-        Alert.error(data.msg, {
-          position: 'top-right',
-          effect: 'slide'
-        });
-      }
-
+      this.msg.show(data.msg, data);
+      console.log(data);
     } );
   },
 
   render: function() {
+    const alertOptions = {
+      offset: 14,
+      position: 'top right',
+      theme: 'light',
+      time: 5000,
+      transition: 'fade',
+      icon: null
+    };
     return (
       <div>
-        <Alert/>
-        <SplitPane split="vertical" defaultSize={280} >
+        <AlertContainer ref={a => this.msg = a} {...alertOptions } />
+        <SplitPane split="vertical" defaultSize={1} >
           <SideBar ref="sideBar"/>
           <Document ref="document"/>
         </SplitPane>
@@ -41,7 +39,7 @@ export default React.createClass({
     );
 
   },
-  
+
   refresh: function() {
     this.refs.sideBar.refresh();
   }
