d38d71bcd1639acb3a1f2af0f124b106cd0b4359
uuinnk <uuinnk@developmentontheedge.com>
2017-11-21 16:10:26 +0700

    move externals to common config, add raw-loader

--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,10 @@ module.exports = {
       }]
     },
     {
+      test: /\.html$/,
+      use: 'raw-loader'
+    },
+    {
       test: /\.css$/,
       loaders: ['style-loader', 'css-loader?importLoaders=1'],
       exclude: ['node_modules']
