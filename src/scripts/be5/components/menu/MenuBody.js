0fdd554d61c38972d5d3d6689faa3449b15511db
uuinnk <uuinnk@developmentontheedge.com>
2017-12-01 15:40:42 +0700

    0.1.34

---
+++ src/scripts/be5/components/menu/MenuBody.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import be5        from '../../be5';
+import MenuNode   from './menuNode';
+import _          from 'underscore';
+
+
+class MenuBody extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {root: [ { title: 'Loading...' } ], query: ''};
+
+    this._getFilteredRoot = this._getFilteredRoot.bind(this);
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  refresh() {
+    be5.net.request('menu', {}, data => {
+      this.setState(data);
+    });
+  }
+
+  render() {
+    const filteredRoot = this._getFilteredRoot();
+    const rootNodes = filteredRoot.map(node => (
+      <MenuNode key={JSON.stringify(node)} data={node} level={1}/>
+    ));
+    return (
+      <div className="menu">{rootNodes}</div>
+    );
+  }
+
+  _getFilteredRoot() {
+    const containsIgnoreCase = (str, substr) => {
+      return str.toLowerCase().indexOf(substr.toLowerCase()) !== -1;
+    };
+    const anyChildContainsIgnoreCase = (node, query) => {
+      if (containsIgnoreCase(node.title, query)) {
+        return true;
+      }
+      return node.children && _.any(node.children, child => anyChildContainsIgnoreCase(child, query));
+    };
+    const filterNodeContent = (node, query) => {
+      if (!node.children) {
+        return node;
+      }
+      return _.extend({}, node, { children: filterByTitle(node.children, query) });
+    };
+    const filterByTitle = (root, query) => {
+      return root
+        .filter(node => anyChildContainsIgnoreCase(node, query))
+        .map(node => filterNodeContent(node, query));
+    };
+
+    return filterByTitle(this.state.root, this.state.query);
+  }
+}
+
+export default MenuBody;
---
+++ src/scripts/be5/components/menu/MenuSearchField.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes            from 'prop-types';
+
+class MenuSearchField extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = { value: ''};
+
+    this._handleChange = this._handleChange.bind(this);
+  }
+
+  render() {
+    return (
+      <input type="text" className="searchField form-control" onChange={this._handleChange} value={this.state.value} placeholder={be5.messages.filter}/>
+    );
+  }
+
+  _handleChange(event) {
+    this.setState({ value: event.target.value });
+    this.props.onChange(event.target.value);
+  }
+}
+
+MenuSearchField.propTypes = {
+  onChange: PropTypes.func.isRequired
+};
+
+export default MenuSearchField;
