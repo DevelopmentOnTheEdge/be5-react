9cdef6d63af8c68cea191b59a9a944a64ed03d69
aavyayzya <aavyayzya@developmentontheedge.com>
2017-12-12 17:44:01 +0700

    create test/wizardEgisso ussue: #22

---
+++ src/scripts/be5/test/wizardEgisso.js
@@ -0,0 +1,28 @@
+import React          from 'react';
+import {be5, changeDocument, FormWizard} from 'be5-react';
+
+class WizardEgisso extends React.Component
+{
+  render() {
+
+    const steps = [
+      {title: be5.messages.companies__Tab, url: '#!companies'},
+      {title: be5.messages.egissoDocs__Tab, url: '#!egissoDocs'},
+      {title: be5.messages.categories__Tab, url: '#!categories'},
+      {title: be5.messages.services__Tab, url: '#!services'},
+    ];
+
+    return (
+      <div className='formWizardTest'>
+        <FormWizard steps={steps} startAtStep={this.props.value.startAtStep}
+          backButtonText={be5.messages.backButtonText}
+          nextButtonText={be5.messages.nextButtonText}
+        />
+      </div>
+    )
+  }
+}
+
+be5.registerAction('wizardEgisso', (documentName) =>{
+  changeDocument(documentName, { component: WizardEgisso, value: {} })
+});
