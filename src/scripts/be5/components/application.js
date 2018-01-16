04e630fb6a02aac696361ee84bd37d11b264bdb8
aavyayzya <aavyayzya@developmentontheedge.com>
2018-01-16 16:56:46 +0700

    change react-alert on react-s-alert, issue #6

--- src/scripts/be5/components/application.js
+++ src/scripts/be5/components/application.js
@@ -3,7 +3,6 @@ import bus from '../core/bus';
 import SideBar from './sideBar';
 import Document from './document';
 import SplitPane from './splitPane';
-import AlertContainer from 'react-alert';
 import Alert from 'react-s-alert';
 
 export default React.createClass({
@@ -15,42 +14,28 @@ export default React.createClass({
     bus.listen('LanguageChanged', this.refresh);
     bus.listen('RoleChanged', this.refresh);
     bus.listen("alert", data => {
-      this.msg.show(data.msg, data);
       console.log(data);
+      if (data.type === 'error') {
+        Alert.error(data.msg, {
+          position: 'top-right',
+          effect: 'slide',
+          timeout: 5000
+        });
+      } else {
+        Alert.success(data.msg, {
+          position: 'top-right',
+          effect: 'slide',
+          timeout: 5000
+        });
+      }
     });
   },
-
-  handleClick(e) {
-    e.preventDefault();
-    Alert.error(
-      bus.listen("alert", data => {
-        this.msg.show(data.msg, data);
-    }), {
-      position: 'top-right',
-      effect: 'slide',
-      timeout: 5000
-    });
-  },
-
   render: function() {
-    const alertOptions = {
-      offset: 14,
-      position: 'top right',
-      theme: 'light',
-      time: 5000,
-      transition: 'fade',
-      icon: null
-    };
     return (
       <div>
-        <AlertContainer ref={a => this.msg = a} {...alertOptions}/>
         <Alert stack={{limit: 3}}/>
         <SplitPane split="vertical" defaultSize={280} >
           <SideBar ref="sideBar"/>
-          <div>
-            <a href="#" onClick={this.handleClick}>Click 1</a>
-          </div>
-
           <Document ref="document" onChange={()=>{}}/>
         </SplitPane>
       </div>
@@ -60,5 +45,5 @@ export default React.createClass({
 
   refresh: function() {
     this.refs.sideBar.refresh();
-  }
+  },
 });
