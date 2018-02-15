ade508e47cb766addcd8069aac702bacbf994e74
uuinnk <uuinnk@developmentontheedge.com>
2018-02-15 14:58:54 +0700

    fix FormWizard, add test

---
+++ test/scripts/be5/components/FormWizard.test.js
@@ -0,0 +1,24 @@
+import React          from 'react';
+import renderer       from 'react-test-renderer';
+import FormWizard     from '../../../../src/scripts/be5/components/FormWizard';
+import be5            from '../../../../src/scripts/be5/be5';
+
+
+test('test', () => {
+  const steps = [
+    {title: "Companies", url: '#!companies'},
+    {title: "EgissoDocs", url: '#!egissoDocs'},
+  ];
+
+  const component = renderer.create(
+    <FormWizard steps={steps}
+                backButtonText={be5.messages.backButtonText}
+                nextButtonText={be5.messages.nextButtonText}
+    />
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+
+  //changeDocument('MainDocument', { value: "Page loaded." } );
+  //expect(component.toJSON()).toMatchSnapshot();
+});
