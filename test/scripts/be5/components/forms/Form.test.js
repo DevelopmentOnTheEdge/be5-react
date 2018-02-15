65c83a2f934929029d9a284143110e939f3a6265
uuinnk <uuinnk@developmentontheedge.com>
2018-02-15 16:40:40 +0700

    add tests

--- test/scripts/be5/components/forms/Form.test.js
+++ test/scripts/be5/components/forms/Form.test.js
@@ -1,46 +1,11 @@
 import React          from 'react';
 import renderer       from 'react-test-renderer';
 import Form           from '../../../../../src/scripts/be5/components/forms/Form';
+import ModalForm           from '../../../../../src/scripts/be5/components/forms/ModalForm';
+import SubmitOnChangeForm           from '../../../../../src/scripts/be5/components/forms/SubmitOnChangeForm';
 
 
 test('Form', () => {
-  const json = {
-
-      "data": {
-        "attributes": {
-          "bean": {
-            "values": {
-              "companyID": ""
-            },
-            "meta": {
-              "/companyID": {
-                "displayName": "companyID"
-              }
-            },
-            "order": [
-              "/companyID"
-            ]
-          },
-          "entity": "companies",
-          "layout": {},
-          "operation": "SelectCompany",
-          "operationResult": {
-            "status": "open"
-          },
-          "query": "Selection view SelectCompany",
-          "selectedRows": "",
-          "title": "SelectCompany"
-        },
-        "type": "form"
-      },
-      "links": {
-        "self": "form/companies/Selection view SelectCompany/SelectCompany"
-      },
-      "meta": {
-        "_ts_": "1507198229686"
-      }
-
-  };
 
   const  component = renderer.create(
     <Form value={json} frontendParams={{documentName: 'test'}} />
@@ -48,3 +13,59 @@ test('Form', () => {
 
   expect(component.toJSON()).toMatchSnapshot();
 });
+
+test('ModalForm', () => {
+
+  const  component = renderer.create(
+    <ModalForm value={json} frontendParams={{documentName: 'test'}} />
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('SubmitOnChangeForm', () => {
+
+  const  component = renderer.create(
+    <SubmitOnChangeForm value={json} frontendParams={{documentName: 'test'}} />
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+const json = {
+
+  "data": {
+    "attributes": {
+      "bean": {
+        "values": {
+          "companyID": ""
+        },
+        "meta": {
+          "/companyID": {
+            "displayName": "companyID"
+          }
+        },
+        "order": [
+          "/companyID"
+        ]
+      },
+      "entity": "companies",
+      "layout": {},
+      "operation": "SelectCompany",
+      "operationResult": {
+        "status": "open"
+      },
+      "query": "Selection view SelectCompany",
+      "selectedRows": "",
+      "title": "SelectCompany"
+    },
+    "type": "form"
+  },
+  "links": {
+    "self": "form/companies/Selection view SelectCompany/SelectCompany"
+  },
+  "meta": {
+    "_ts_": "1507198229686"
+  }
+
+};
--- test/scripts/be5/components/tables/Table.test.js
+++ test/scripts/be5/components/tables/Table.test.js
@@ -1,73 +1,99 @@
 import React          from 'react';
 import renderer       from 'react-test-renderer';
-import Table           from '../../../../../src/scripts/be5/components/tables/Table';
+import Table          from '../../../../../src/scripts/be5/components/tables/Table';
+import TableForm      from '../../../../../src/scripts/be5/components/tables/TableForm';
+import FormTable      from '../../../../../src/scripts/be5/components/tables/FormTable';
+import TableFormRow   from '../../../../../src/scripts/be5/components/tables/TableFormRow';
 
 test('Table', () => {
-
-  const json = {
-    "data": {
-      "attributes": {
-        "category": "companies",
-        "columns": [
-          "Наименование"
-        ],
-        "hasAggregate": false,
-        "layout": {
-          "type": "tableForm"
-        },
-        "length": 1,
-        "operations": [
-          {
-            "clientSide": false,
-            "isClientSide": false,
-            "name": "Edit",
-            "requiresConfirmation": false,
-            "title": "Редактировать",
-            "visibleWhen": "oneSelected"
-          },
-          {
-            "clientSide": false,
-            "isClientSide": false,
-            "name": "Insert",
-            "requiresConfirmation": false,
-            "title": "Добавить",
-            "visibleWhen": "always"
-          }
-        ],
-        "page": "Общие сведения",
-        "parameters": {},
-        "rows": [
-          {
-            "cells": [
-              {
-                "content": "adfd",
-                "options": {
-                  "quick": {
-                    "visible": "false"
-                  }
-                }
-              }
-            ],
-            "id": "19"
-          }
-        ],
-        "selectable": true,
-        "title": "Организации: Общие сведения",
-        "totalNumberOfRows": 1
-      },
-      "type": "table"
-    },
-    "links": {
-      "self": "table/companies/Общие сведения"
-    },
-    "meta": {
-      "_ts_": "1507198487234"
-    }
-  };
-
   const  component = renderer.create(
     <Table value={json}/>
   );
 
   expect(component.toJSON()).toMatchSnapshot();
 });
+
+test('TableForm', () => {
+  const  component = renderer.create(
+    <TableForm value={json}/>
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('FormTable', () => {
+  const  component = renderer.create(
+    <FormTable value={json}/>
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('TableFormRow', () => {
+  const  component = renderer.create(
+    <TableFormRow value={json}/>
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+const json = {
+  "data": {
+    "attributes": {
+      "category": "companies",
+      "columns": [
+        "Наименование"
+      ],
+      "hasAggregate": false,
+      "layout": {
+        "type": "tableForm"
+      },
+      "length": 1,
+      "operations": [
+        {
+          "clientSide": false,
+          "isClientSide": false,
+          "name": "Edit",
+          "requiresConfirmation": false,
+          "title": "Редактировать",
+          "visibleWhen": "oneSelected"
+        },
+        {
+          "clientSide": false,
+          "isClientSide": false,
+          "name": "Insert",
+          "requiresConfirmation": false,
+          "title": "Добавить",
+          "visibleWhen": "always"
+        }
+      ],
+      "page": "Общие сведения",
+      "parameters": {},
+      "rows": [
+        {
+          "cells": [
+            {
+              "content": "adfd",
+              "options": {
+                "quick": {
+                  "visible": "false"
+                }
+              }
+            }
+          ],
+          "id": "19"
+        }
+      ],
+      "selectable": true,
+      "title": "Организации: Общие сведения",
+      "totalNumberOfRows": 1
+    },
+    "type": "table"
+  },
+  "links": {
+    "self": "table/companies/Общие сведения"
+  },
+  "meta": {
+    "_ts_": "1507198487234"
+  }
+};
