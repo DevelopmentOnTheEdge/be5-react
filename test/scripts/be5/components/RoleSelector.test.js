ed08be485064f4d686df76ce226392ded8b98f4a
uuinnk <uuinnk@developmentontheedge.com>
2018-02-16 12:34:35 +0700

    add tests

--- test/scripts/be5/components/RoleSelector.test.js
+++ test/scripts/be5/components/RoleSelector.test.js
@@ -21,7 +21,7 @@ test('calls', () => {
 
   be5.net.request = function (path, attr, callback) {
     handle(path, attr, callback);
-    callback({"availableRoles":["Administrator", "Manager"],"selectedRoles":["Manager"]})
+    callback({"availableRoles":["Administrator", "Manager", "Role1", "Role2"],"selectedRoles":["Manager", "Role1"]})
   };
 
   const wrapper = mount(
@@ -30,21 +30,24 @@ test('calls', () => {
   expect(handle.mock.calls.length).toBe(1);
   expect(handle.mock.calls[0]).toEqual(["roleSelector", {}, expect.any(Function)]);
 
-  expect(wrapper.find('input[type="checkbox"]').length).toEqual(2);
+  expect(wrapper.find('input[type="checkbox"]').length).toEqual(4);
   expect(wrapper.find('#Administrator-checkbox').props().checked).toEqual(false);
   expect(wrapper.find('#Manager-checkbox').props().checked).toEqual(true);
 
-  wrapper.find('input[type="checkbox"]').first().simulate('change');
-
+  wrapper.find('#Administrator-checkbox').first().simulate('change');
   expect(handle.mock.calls[1]).toEqual(
-    ["roleSelector/select", {"roles": "Manager,Administrator"}, expect.any(Function)]);
+    ["roleSelector/select", {"roles": "Manager,Role1,Administrator"}, expect.any(Function)]);
+
+  wrapper.find('#Manager-checkbox').first().simulate('change');
+  expect(handle.mock.calls[2]).toEqual(
+    ["roleSelector/select", {"roles": "Role1"}, expect.any(Function)]);
 
   wrapper.find('.enable-all').simulate('click');
-  expect(handle.mock.calls[2]).toEqual(
-    ["roleSelector/select", {"roles": "Administrator,Manager"}, expect.any(Function)]);
+  expect(handle.mock.calls[3]).toEqual(
+    ["roleSelector/select", {"roles": "Administrator,Manager,Role1,Role2"}, expect.any(Function)]);
 
   wrapper.find('.disable-all').simulate('click');
-  expect(handle.mock.calls[3]).toEqual(
+  expect(handle.mock.calls[4]).toEqual(
     ["roleSelector/select", {"roles": ""}, expect.any(Function)]);
 
 });
