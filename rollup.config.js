79b95eae81ecffd0276509de0333dbc7e821d71b
uuinnk <uu.innk@gmail.com>
2018-02-22 14:18:59 +0700

    remove unused

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,13 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
-import babel    from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-//import globals from 'rollup-plugin-node-globals'
-import replace from 'rollup-plugin-replace'
-import resolve from 'rollup-plugin-node-resolve'
-//import image   from 'rollup-plugin-image';
+import babel       from 'rollup-plugin-babel'
+import commonjs    from 'rollup-plugin-commonjs'
+import replace     from 'rollup-plugin-replace'
+import resolve     from 'rollup-plugin-node-resolve'
 import imageBase64 from 'rollup-plugin-image-base64';
-import postcss from 'rollup-plugin-postcss'
-//import inject  from 'rollup-plugin-inject'
-
-import pkg from './package.json'
+import pkg         from './package.json'
 
 const external = Object.keys(pkg.dependencies || {});
 
-
-
-//const file = 'dist/lib/be5-react.es.js';
 
 export default {
   input: 'src/scripts/be5/index.js',
@@ -27,7 +19,6 @@ export default {
   name: pkg.main,
   plugins: [
     nodeResolve(),
-    postcss({ extract: true }),
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
