24aa6f0232c538a412ebf2b8fb3ae02497ef83cc
uuinnk <uuinnk@developmentontheedge.com>
2018-03-13 12:45:20 +0700

    remove outer div in inline form

--- src/scripts/be5/components/forms/InlineForm.js
+++ src/scripts/be5/components/forms/InlineForm.js
@@ -3,26 +3,31 @@ import classNames      from 'classnames';
 import be5             from '../../be5';
 import Form            from './Form';
 import formsCollection from '../../services/formsCollection';
-import PropertySet     from 'beanexplorer-react';
+import {Property}      from 'beanexplorer-react';
 
 
 class InlineForm extends Form
 {
   render() {
     const attributes = this.state.data.attributes;
+    const commonProps = {
+      bean: attributes.bean,
+      onChange: this._onFieldChange,
+      localization: be5.messages.property,
+      inline: true,
+      rowClass:"d-flex",
+    };
+
+    const properties = attributes.bean.order.map(p => (
+      <Property key={p} path={p} {...commonProps} />
+    ));
 
     return (
       <form
         onSubmit={this._applyOnSubmit}
         className={classNames('form-inline', attributes.cssClass, this.state.wasValidated ? 'was-validated' : '')}
       >
-        <PropertySet
-          bean={attributes.bean}
-          onChange={this._onFieldChange}
-          localization={be5.messages.property}
-          inline
-          rowClass="d-flex"
-        />
+        {properties}
         {this._createOkAction('mb-2')}
         {this._getErrorPane()}
       </form>
