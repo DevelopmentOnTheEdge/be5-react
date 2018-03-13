fb6bf68a05680ca27e95fb7b5885a06d3f604da0
uuinnk <uuinnk@developmentontheedge.com>
2018-03-13 16:20:48 +0700

    fix css order

--- src/scripts/be5/be5styles.js
+++ src/scripts/be5/be5styles.js
@@ -5,6 +5,7 @@ import 'react-virtualized-select/styles.css';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
 
+import 'beanexplorer-react/src/propertySet.css';
 import '../../sass/styles.scss';
 import '../../css/base.css';
 import '../../css/document.css';
@@ -12,7 +13,6 @@ import '../../css/splitPane.css';
 import '../../css/form.css';
 import '../../css/table.css';
 import '../../css/formWizard.css';
-import 'beanexplorer-react/src/propertySet.css';
 import '../../css/errorPane.css';
 import '../../css/qBuilder.css';
 import '../../css/roleSelector.css';
