6f4d5a813177321aa5fc0321789e1c7c60fe912f
uuinnk <uuinnk@developmentontheedge.com>
2018-03-21 12:53:03 +0700

    stateless

--- src/scripts/be5/components/RoleBox.js
+++ src/scripts/be5/components/RoleBox.js
@@ -24,18 +24,11 @@ Role.propTypes = {
   onChange: PropTypes.func.isRequired
 };
 
-class RoleBox extends React.Component {
-  constructor(props) {
-    super(props);
+const RoleBox = (props) => {
 
-    this.onRoleChange = this.onRoleChange.bind(this);
-    this.handleSelectAll = this.handleSelectAll.bind(this);
-    this.handleClear = this.handleClear.bind(this);
-  }
-
-  onRoleChange(name)
+  function onRoleChange(name)
   {
-    let roles = [...this.props.selectedRoles];
+    let roles = [...props.selectedRoles];
     let containRoleIndex = roles.indexOf(name);
 
     if ( containRoleIndex !== -1) {
@@ -44,46 +37,43 @@ class RoleBox extends React.Component {
       roles.push(name);
     }
 
-    this.props.toggleRoles(roles.join(","));
+    props.toggleRoles(roles.join(","));
   }
 
-  handleSelectAll() {
-    this.props.toggleRoles(this.props.availableRoles.join(","))
+  function handleSelectAll() {
+    props.toggleRoles(props.availableRoles.join(","))
   }
 
-  handleClear() {
-    this.props.toggleRoles("")
+  function handleClear() {
+    props.toggleRoles("")
   }
 
-  render() {
-    if (this.props.availableRoles.length < 1)
-    {
-      return ( <div/> );
-    }
-
-    const roleNodes = this.props.availableRoles.map((role) =>
-      <Role key={role} name={role} checked={this.props.selectedRoles.indexOf(role) !== -1} onChange={() => this.onRoleChange(role)}/>
-    );
-
-    return (
-      <UncontrolledDropdown size={this.props.size} className="roleBox mr-sm-2">
-        <DropdownToggle caret>{be5.messages.roles}</DropdownToggle>
-
-        <DropdownMenu>
-          {roleNodes}
-          <DropdownItem divider />
-
-          <div className="roleBox_add-actions">
-            Выбрать:{' '}
-            <Button onClick={this.handleSelectAll} color="primary" className="enable-all" size="sm">Всё</Button>{' '}
-            <Button onClick={this.handleClear} color="secondary" className="disable-all" size="sm">Ничего</Button>
-          </div>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    );
+  if(props.availableRoles.length < 1)
+  {
+    return ( <div/> );
   }
 
-}
+  const roleNodes = props.availableRoles.map((role) =>
+    <Role key={role} name={role} checked={props.selectedRoles.indexOf(role) !== -1} onChange={() => onRoleChange(role)}/>
+  );
+
+  return (
+    <UncontrolledDropdown size={props.size} className="roleBox mr-sm-2">
+      <DropdownToggle caret>{be5.messages.roles}</DropdownToggle>
+
+      <DropdownMenu>
+        {roleNodes}
+        <DropdownItem divider />
+
+        <div className="roleBox_add-actions">
+          Выбрать:{' '}
+          <Button onClick={handleSelectAll} color="primary" className="enable-all" size="sm">Всё</Button>{' '}
+          <Button onClick={handleClear} color="secondary" className="disable-all" size="sm">Ничего</Button>
+        </div>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  );
+};
 
 RoleBox.propTypes = {
   size: PropTypes.string,
