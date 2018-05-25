b00564689b7e5d4dd4b5c4c4c61e79391d0c8646
uuinnk <uuinnk@developmentontheedge.com>
2018-05-25 13:19:10 +0700

    add Be5Components

--- src/scripts/be5/components/MainDocumentOnly.js
+++ src/scripts/be5/components/MainDocumentOnly.js
@@ -1,11 +1,13 @@
 import React         from 'react';
 import be5           from '../be5';
 import Document      from '../containers/Document';
+import Be5Components from "./Be5Components";
 
 
 const MainDocumentOnly = () => {
   return (
     <div className="MainDocument-only">
+      <Be5Components/>
       <Document frontendParams={{documentName: be5.MAIN_DOCUMENT}} />
     </div>
   );
