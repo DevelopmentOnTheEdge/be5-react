b5b3bede8ab9da93e343c02d0177c38ae0f00235
uuinnk <uuinnk@developmentontheedge.com>
2018-08-07 15:33:22 +0700

    add console.trace()

--- src/scripts/be5/utils/preconditions.js
+++ src/scripts/be5/utils/preconditions.js
@@ -11,12 +11,14 @@ const createArgumentEqualityError = message => ({
 export default {
   passed: function(argument, message) {
     if (!argument) {
+      console.trace();
       throw createMandatoryArgumentError(message || 'argument is missing');
     }
     return argument;
   },
   eq: function(arg1, arg2, message) {
     if (arg1 !== arg2) {
+      console.trace();
       throw createArgumentEqualityError(message || `${arg1} should be equal to ${arg2}`);
     }
   }
