a00478c823603fa9d077e7b091bbcc4510f69f0f
Nikita Mandrik <manikitos@developmentontheedge.com>
2019-07-31 17:04:35 +0700

    build

--- dist/lib/be5-react.js
+++ dist/lib/be5-react.js
@@ -566,6 +566,13 @@ var addUrlHandlers = function addUrlHandlers(element, documentName) {
       openInModal(e);
     }
   });
+
+  element.on("click", '.close-modal', function (e) {
+    if (!e.ctrlKey) {
+      e.preventDefault();
+      bus.fire("mainModalClose");
+    }
+  });
 };
 
 var processHashUrlForDocument = function processHashUrlForDocument(e, documentName) {
@@ -624,157 +631,157 @@ var getDefaultCancelAction = function getDefaultCancelAction() {
 };
 
 var messages = {
-    en: {
-        errorCannotConnect: 'Cannot connect to server',
-        errorServerQueryException: 'Error during server query: $message',
-        errorInvalidErrorResponse: 'Server returned unknown error',
-        errorNoData: 'Error communicating with server: no data received',
-        errorUnknownRoute: 'Unknown route: $action',
-        errorUrlParameterAbsent: 'Invalid URL: $parameter is absent',
+  en: {
+    errorCannotConnect: 'Cannot connect to server',
+    errorServerQueryException: 'Error during server query: $message',
+    errorInvalidErrorResponse: 'Server returned unknown error',
+    errorNoData: 'Error communicating with server: no data received',
+    errorUnknownRoute: 'Unknown route: $action',
+    errorUrlParameterAbsent: 'Invalid URL: $parameter is absent',
 
-        welcome: 'Hello!',
-        loading: 'Page is loading...',
-        settings: 'Settings',
-        otherColumns: 'Other columns',
-        roles: 'Roles',
-        back: 'Back',
-        error: 'Error:',
-        cancel: 'Cancel',
-        close: 'Close',
-        login: 'Login',
-        logout: 'Logout',
-        reload: 'reload',
-        All: 'All',
-        successfullyCompleted: 'Successfully completed.',
+    welcome: 'Hello!',
+    loading: 'Page is loading...',
+    settings: 'Settings',
+    otherColumns: 'Other columns',
+    roles: 'Roles',
+    back: 'Back',
+    error: 'Error:',
+    cancel: 'Cancel',
+    close: 'Close',
+    login: 'Login',
+    logout: 'Logout',
+    reload: 'reload',
+    All: 'All',
+    successfullyCompleted: 'Successfully completed.',
 
-        filter: 'Filter...',
-        entries: 'entries',
+    filter: 'Filter...',
+    entries: 'entries',
 
-        selectRoles: 'Select',
-        allRoles: 'all',
-        clearRoles: 'clear',
+    selectRoles: 'Select',
+    allRoles: 'all',
+    clearRoles: 'clear',
 
-        Submit: 'Submit',
-        submitted: 'In progress...',
+    Submit: 'Submit',
+    submitted: 'In progress...',
 
-        formComponentNotFound: 'Document component not found: ',
-        tableComponentNotFound: 'Table component not found: ',
-        componentForTypeNotRegistered: 'Component for type "$type" is not registered.',
-        tableBoxForTypeNotRegistered: 'TableBox for type "$type" is not registered.',
+    formComponentNotFound: 'Document component not found: ',
+    tableComponentNotFound: 'Table component not found: ',
+    componentForTypeNotRegistered: 'Component for type "$type" is not registered.',
+    tableBoxForTypeNotRegistered: 'TableBox for type "$type" is not registered.',
 
-        helpInfo: "Help",
-        details: "Details",
-        goToHomepage: "Go to homepage",
+    helpInfo: "Help",
+    details: "Details",
+    goToHomepage: "Go to homepage",
 
-        NotFound: "Not Found",
+    NotFound: "Not Found",
 
-        table: {
-            noRecordsOnThePage: 'No records on page {0}',
-            emptyTable: 'Nothing found',
-            previousPage: 'Previous',
-            nextPage: 'Next',
-            firstPage: 'First',
-            lastPage: 'Last',
-            filter: 'Filter',
-            clearFilter: 'Clear',
-            tableFor: 'for'
-        }
+    table: {
+      noRecordsOnThePage: 'No records on page {0}',
+      emptyTable: 'Nothing found',
+      previousPage: 'Previous',
+      nextPage: 'Next',
+      firstPage: 'First',
+      lastPage: 'Last',
+      filter: 'Filter',
+      clearFilter: 'Clear',
+      tableFor: 'for'
+    }
+  },
+
+  ru: {
+    errorCannotConnect: 'Не могу подключиться к серверу',
+    errorServerQueryException: 'Ошибка сервера: $message',
+    errorInvalidErrorResponse: 'Сервер вернул неизвестную ошибку',
+    errorNoData: 'Ошибка связи с сервером: ответ не получен',
+    errorUnknownRoute: 'Неизвестный путь: $action',
+    errorUrlParameterAbsent: 'Неверный URL: отсутствует $parameter',
+
+    welcome: 'Добро пожаловать!',
+    loading: 'Загрузка...',
+    settings: 'Настройки',
+    otherColumns: 'Другие колонки',
+    roles: 'Роли',
+    back: 'Назад',
+    error: 'Ошибка:',
+    cancel: 'Отмена',
+    close: 'Закрыть',
+    login: 'Вход',
+    logout: 'Выход',
+    reload: 'Перезагрузить',
+    All: 'Все',
+    successfullyCompleted: 'Успешно выполнено.',
+
+    filter: 'Фильтр...',
+    entries: 'записей',
+
+    selectRoles: 'Выбрать',
+    allRoles: 'Всё',
+    clearRoles: 'Ничего',
+
+    Submit: 'Выполнить',
+    submitted: 'Выполняется...',
+
+    property: {
+      locale: 'ru',
+      clearAllText: 'Очистить всё',
+      clearValueText: 'Очистить',
+      noResultsText: 'Нет результатов',
+      searchPromptText: 'Начните вводить для поиска',
+      placeholder: 'Выберите...',
+      loadingPlaceholder: 'Загрузка...',
+      stepMismatch: 'Введите допустимое значение. Ближайшие допустимые значения: {0} and {1}.',
+      numberTypeMismatch: 'Введите число.',
+      simpleIntegerTypeMismatch: '"E" не поддерживается для простых целых типов.',
+      rangeOverflow: 'Значение должно быть меньше или равно {0}.',
+      rangeUnderflow: 'Значение должно быть больше или равно {0}.',
+      datePatternError: 'Введите дату в формате дд.мм.гггг',
+      timestampPatternError: 'Введите дату и время в формате дд.мм.гггг чч:мм'
     },
 
-    ru: {
-        errorCannotConnect: 'Не могу подключиться к серверу',
-        errorServerQueryException: 'Ошибка сервера: $message',
-        errorInvalidErrorResponse: 'Сервер вернул неизвестную ошибку',
-        errorNoData: 'Ошибка связи с сервером: ответ не получен',
-        errorUnknownRoute: 'Неизвестный путь: $action',
-        errorUrlParameterAbsent: 'Неверный URL: отсутствует $parameter',
+    formComponentNotFound: 'Компонент формы не найден: ',
+    tableComponentNotFound: 'Компонент таблицы не найден: ',
+    componentForTypeNotRegistered: 'Компонент для типа "$type" не зарегистрирован.',
+    tableBoxForTypeNotRegistered: 'TableBox для типа "$type" не зарегистрирован.',
 
-        welcome: 'Добро пожаловать!',
-        loading: 'Загрузка...',
-        settings: 'Настройки',
-        otherColumns: 'Другие колонки',
-        roles: 'Роли',
-        back: 'Назад',
-        error: 'Ошибка:',
-        cancel: 'Отмена',
-        close: 'Закрыть',
-        login: 'Вход',
-        logout: 'Выход',
-        reload: 'Перезагрузить',
-        All: 'Все',
-        successfullyCompleted: 'Успешно выполнено.',
+    helpInfo: "Справка",
+    details: "Подробнее",
+    goToHomepage: "Перейти на главную страницу",
 
-        filter: 'Фильтр...',
-        entries: 'записей',
-
-        selectRoles: 'Выбрать',
-        allRoles: 'Всё',
-        clearRoles: 'Ничего',
-
-        Submit: 'Выполнить',
-        submitted: 'Выполняется...',
-
-        property: {
-            locale: 'ru',
-            clearAllText: 'Очистить всё',
-            clearValueText: 'Очистить',
-            noResultsText: 'Нет результатов',
-            searchPromptText: 'Начните вводить для поиска',
-            placeholder: 'Выберите...',
-            loadingPlaceholder: 'Загрузка...',
-            stepMismatch: 'Введите допустимое значение. Ближайшие допустимые значения: {0} and {1}.',
-            numberTypeMismatch: 'Введите число.',
-            simpleIntegerTypeMismatch: '"E" не поддерживается для простых целых типов.',
-            rangeOverflow: 'Значение должно быть меньше или равно {0}.',
-            rangeUnderflow: 'Значение должно быть больше или равно {0}.',
-            datePatternError: 'Введите дату в формате дд.мм.гггг',
-            timestampPatternError: 'Введите дату и время в формате дд.мм.гггг чч:мм'
-        },
-
-        formComponentNotFound: 'Компонент формы не найден: ',
-        tableComponentNotFound: 'Компонент таблицы не найден: ',
-        componentForTypeNotRegistered: 'Компонент для типа "$type" не зарегистрирован.',
-        tableBoxForTypeNotRegistered: 'TableBox для типа "$type" не зарегистрирован.',
-
-        helpInfo: "Справка",
-        details: "Подробнее",
-        goToHomepage: "Перейти на главную страницу",
-
-        NotFound: "Не найдено",
-        table: {
-            noRecordsOnThePage: 'Нет записей на {0} странице',
-            emptyTable: 'Нет данных',
-            previousPage: 'Предыдущая',
-            nextPage: 'Следующая',
-            firstPage: 'Первая',
-            lastPage: 'Последняя',
-            filter: 'Фильтр',
-            clearFilter: 'Очистить',
-            tableFor: 'для'
-        },
-        dataTables: {
-            "processing": "Подождите...",
-            "search": "Поиск:",
-            "lengthMenu": "Показать _MENU_ записей",
-            "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
-            "infoEmpty": "Записи с 0 до 0 из 0 записей",
-            "infoFiltered": "(отфильтровано из _MAX_ записей)",
-            "infoPostFix": "",
-            "loadingRecords": "Загрузка записей...",
-            "zeroRecords": "Записи отсутствуют.",
-            "emptyTable": "В таблице отсутствуют данные",
-            "paginate": {
-                "first": "Первая",
-                "previous": "Предыдущая",
-                "next": "Следующая",
-                "last": "Последняя"
-            },
-            "aria": {
-                "sortAscending": ": активировать для сортировки столбца по возрастанию",
-                "sortDescending": ": активировать для сортировки столбца по убыванию"
-            }
-        }
+    NotFound: "Не найдено",
+    table: {
+      noRecordsOnThePage: 'Нет записей на {0} странице',
+      emptyTable: 'Нет данных',
+      previousPage: 'Предыдущая',
+      nextPage: 'Следующая',
+      firstPage: 'Первая',
+      lastPage: 'Последняя',
+      filter: 'Фильтр',
+      clearFilter: 'Очистить',
+      tableFor: 'для'
+    },
+    dataTables: {
+      "processing": "Подождите...",
+      "search": "Поиск:",
+      "lengthMenu": "Показать _MENU_ записей",
+      "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+      "infoEmpty": "Записи с 0 до 0 из 0 записей",
+      "infoFiltered": "(отфильтровано из _MAX_ записей)",
+      "infoPostFix": "",
+      "loadingRecords": "Загрузка записей...",
+      "zeroRecords": "Записи отсутствуют.",
+      "emptyTable": "В таблице отсутствуют данные",
+      "paginate": {
+        "first": "Первая",
+        "previous": "Предыдущая",
+        "next": "Следующая",
+        "last": "Последняя"
+      },
+      "aria": {
+        "sortAscending": ": активировать для сортировки столбца по возрастанию",
+        "sortDescending": ": активировать для сортировки столбца по убыванию"
+      }
     }
+  }
 };
 
 var routes = {};
