aca8f5c0aad82095611b97fe2d2b1933c93e892b
mixa <mixa@developmentontheedge.com>
2020-11-27 17:14:50 +0700

    mock isGuest function, return false

--- test/jestGlobals.js
+++ test/jestGlobals.js
@@ -3,6 +3,7 @@ import _        from 'underscore';
 import 'jest-canvas-mock'
 import Adapter from 'enzyme-adapter-react-16';
 import {configure} from 'enzyme';
+import * as utils from "../src/scripts/be5/utils/utils"
 configure({adapter: new Adapter()});
 
 
@@ -10,3 +11,4 @@ global._ = _;
 global.$ = $;
 
 window.URL.createObjectURL = function () {};
+jest.spyOn(utils, "isGuest").mockReturnValue(false);
