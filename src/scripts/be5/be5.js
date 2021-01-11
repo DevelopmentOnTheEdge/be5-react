9735267434d16d6bc81e81d1c9dc8433fb714ef3
mixa <mixa@developmentontheedge.com>
2021-01-11 19:06:45 +0700

    clean

--- src/scripts/be5/be5.js
+++ src/scripts/be5/be5.js
@@ -122,8 +122,6 @@ const be5 = {
       for (let i = 0; i < positional.length; i++) {
         res.push(encodeURIComponent(positional[i]));
       }
-      console.log(`named`)
-      console.log(named)
       for (let key in named) {
         res.push(key + '=' + encodeURIComponent(named[key]));
       }
