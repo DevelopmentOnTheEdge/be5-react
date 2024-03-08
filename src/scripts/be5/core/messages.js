0f01cfc8cdcc75e9a3f642463f897a1606d090b6
Curiod <lom3huza@gmail.com>
2024-03-08 21:58:33 +0300

    build

--- src/scripts/be5/core/messages.js
+++ src/scripts/be5/core/messages.js
@@ -153,5 +153,18 @@ export default {
         "sortDescending": ": активировать для сортировки столбца по убыванию"
       }
     }
+  },
+
+
+  zh: {
+    welcome: '欢迎!',
+
+    login: '登录',
+    logout: '退出',
+    roles: '角色',
+
+    allRoles: '一切',
+    clearRoles: '莫',
+
   }
 };
