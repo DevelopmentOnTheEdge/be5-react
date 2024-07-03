76118e364b878b371392a883e4397b95b6650078
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2024-07-03 14:42:17 +0700

    functional style

--- src/scripts/be5/components/tables/tableBoxes/JsonFormatTableBox.js
+++ src/scripts/be5/components/tables/tableBoxes/JsonFormatTableBox.js
@@ -1,26 +1,28 @@
-import React, {Component} from 'react';
-import {registerTableBox} from "../../../core/registers/tableBoxes";
+import React from 'react';
+import { registerTableBox } from "../../../core/registers/tableBoxes";
 
-class JsonFormatTableBox extends Component {
-    render() {
-        const {columns, rows, parameters, layout} = this.props.value.data.attributes;
-        const names = columns.map(col => col.name);
-        const array = rows.map(row => {
-            const obj = {};
-            names.forEach((name, idx) => {
-                if (row.cells.length - 1 >= idx) {
-                    obj[name] = row.cells[idx].content;
-                }
-            })
-            return obj;
-        })
-        const prettyJson = parameters._prettyJson_ === 'yes' || layout._prettyJson_ === 'yes';
-        const content = prettyJson ?
-            <pre className="jsonTableBox" dangerouslySetInnerHTML={{__html: JSON.stringify(array, null, 4)}}/> :
-            <div className="jsonTableBox" dangerouslySetInnerHTML={{__html: JSON.stringify(array)}}/>;
-        return (content);
-    }
-}
+const JsonFormatTableBox = ({ value }) => {
+  const { columns, rows, parameters, layout } = value.data.attributes;
+  const names = columns.map(col => col.name);
+  const array = rows.map(row => {
+    const obj = {};
+    names.forEach((name, idx) => {
+      if (row.cells.length - 1 >= idx) {
+        obj[name] = row.cells[idx].content;
+      }
+    });
+    return obj;
+  });
+
+  const prettyJson = parameters._prettyJson_ === 'yes' || layout._prettyJson_ === 'yes';
+  const content = prettyJson ? (
+    <pre className="jsonTableBox" dangerouslySetInnerHTML={{ __html: JSON.stringify(array, null, 4) }} />
+  ) : (
+    <div className="jsonTableBox" dangerouslySetInnerHTML={{ __html: JSON.stringify(array) }} />
+  );
+
+  return content;
+};
 
 registerTableBox('json', JsonFormatTableBox);
 
